e685b1b7959efdfbe693717bae6b9496f49e1aa1
xixi <coderxixi@coderdeMacBook-Pro.local>
2022-05-24 20:23:19 +0800

    组件渲染流程

---
+++ src/runtime-core/createApp.ts
@@ -0,0 +1,17 @@
+import { render } from "./render";
+import { createVNode } from "./vnode"
+
+export function createApp(rootComponent){
+     return {
+         mount(rootContainer){
+             // 先转化成虚拟节点（vnode)
+             //把组件（compontent）转化成虚拟节点（v node）
+             //所有的逻辑操作都会基于这个虚拟节点（v node）做处理
+
+             const vnode=createVNode(rootComponent);
+
+             render(vnode,rootContainer)
+         }
+     }
+}
+
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -8,11 +8,37 @@ export function render(vnode,container){
  }
 
  function path(vnode,container){
-    //处理组件
-    processComponent(vnode,container);
-    //判断是不是元素类型(element)
+      //判断是不是元素类型(element)
+   
+    //处理组件 如何区分element还是component 类型
+    if(vnode.type=='string'){
+      processElement(vnode ,container)
+    }else if(vnode.tyoe=='object'){
+      processComponent(vnode,container);
+    }
+    
+   
  }
-
+function processElement(vnode:any,container:any){
+  mountElement(vnode,container)
+    
+}
+function mountElement(vnode:any,container:any){
+  const el=document.createElement(vnode.type);
+  const {children,props} =vnode;
+  //处理children
+  if(typeof children=='string'){
+    el.textContent=children;
+  }else if(Array.isArray(children)){
+    mountChidren(vnode,el)
+  }
+  
+  for(const key in props){
+    const val=props[key];
+    el.setAtteribute(key,val)
+  }
+  container.append(el)
+}
  function processComponent(vnode:any,container:any){
      //挂载组件
      mountComponent(vnode,container)
@@ -30,4 +56,10 @@ function setuoRenderEffect(instance:any,container:any){
     //虚拟节点树（vnode 元素类型  mountElement）path 
     const subTree=instance.render();
     path(subTree,container)
+}
+
+function mountChidren(vnode:any,container:any){
+       vnode.children.forEach(function(v){
+         path(v,container)
+       })
 }
