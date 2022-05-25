8a58be12f5fe2390e96d7ace2523d3cfd7720fe7
xixi <coderxixi@coderdeMacBook-Pro.local>
2022-05-25 21:10:15 +0800

    作用域插槽

--- src/runtime-core/compontentSlots.ts
+++ src/runtime-core/compontentSlots.ts
@@ -2,7 +2,7 @@ export function initSlots(inistance,childrn){
     const slots={}
     for(const key in childrn){
         const value=childrn[key];
-        slots[key]=normalizeSlotValeue(value)
+        slots[key]=(props)=>normalizeSlotValeue(value(props))
     }
     inistance.slots=slots
 }
