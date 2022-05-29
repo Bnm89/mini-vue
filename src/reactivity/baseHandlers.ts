69eb0a9beaf215f52126a53f78168567a9165488
liuss7 <liuss7@asiainfo.com>
2022-05-29 11:54:12 +0800

    实现reactive和readonly嵌套对象转化功能

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,6 +1,7 @@
 
+import { isObject } from '../shared'
 import { track, trigger } from './effect'
-import { ReactiveFlags } from './reactive'
+import { ReactiveFlags ,reactive,readonly} from './reactive'
 const get = createGetter()
 const set = createSttter()
 const readonlyGet = createGetter(true)
@@ -9,7 +10,6 @@ export const mutableHandler = {
     get,
     set,
 }
-
 export const readonlyHandler = {
     readonlyGet,
     set(target, key, val) {
@@ -17,10 +17,9 @@ export const readonlyHandler = {
         return true
     }
 }
-
 function createGetter(isReadonly = false) {
     return function get(target, key) {
-        if (key == ReactiveFlags.IS_REACTIVE) {
+    if (key == ReactiveFlags.IS_REACTIVE) {
             return !isReadonly
         } else if (key == ReactiveFlags.IS_READONLY) {
             return isReadonly
@@ -28,7 +27,12 @@ function createGetter(isReadonly = false) {
         if (!isReadonly) {
             track(target, key)
         }
+        //判断res是不是一个object 
+        
         const res = Reflect.get(target, key);
+        if(isObject(res)){
+          return isReadonly?readonly(res): reactive(res)
+        }
         //依赖收集
         return res
     }
